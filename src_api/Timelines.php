512e423b63bc378b991452149c47c66c0346ea92
mojoaxel <alex@wunschik.net>
2014-10-26 16:54:38 +0100

    api: added user user-agent for geting ffSummerizedDir.json

--- src_api/Timelines.php
+++ src_api/Timelines.php
@@ -1,7 +1,16 @@
 <?php
 $jsonurl = "https://freifunk.net/map/ffSummarizedDir.json";
 
-$ffSummarizedDir_json = file_get_contents($jsonurl, 0, null, null);
+$opts = array(
+	'http'=>array(
+			'method'=>"GET",
+			'header'=>
+				"Accept-language: en\r\n" .
+				"User-Agent: Mozilla/5.0 (Windows NT 5.1; rv:23.0) Gecko/20100101 Firefox/23.0\r\n"
+	)
+);
+$context = stream_context_create($opts);
+$ffSummarizedDir_json = file_get_contents($jsonurl, false, $context);
 $ffSummarizedDir = json_decode($ffSummarizedDir_json);
 $timelines = array();
 
